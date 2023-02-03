af764d8278b9edb59953be6f3fdf54b982b9d608
hamid <55974792+Raihanna1013@users.noreply.github.com>
2023-02-04 01:12:14 +0700

    Ui baru

--- app/src/main/java/com/example/eppidbawaslu/SplashActivity.kt
+++ app/src/main/java/com/example/eppidbawaslu/SplashActivity.kt
@@ -9,23 +9,23 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
-        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
 
+        val motionLayout= findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
             }
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
             }
 
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                startActivity(Intent(this@SplashActivity, Home::class.java))
                 finish()
             }
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
             }
-
         })
     }
 }
